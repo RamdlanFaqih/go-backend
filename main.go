e51f9c9a8364b662b428f66323bf8c66aa859dd8
RamdlanFaqih <ramfaqih@gmail.com>
2023-12-20 16:36:19 +0700

    list/get data

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type product struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+	Stock int    `json:"stock"`
+}
+
+var products = []product{
+	product{1, "baju", 100000, 90},
+	product{2, "kemeja", 10000, 20},
+}
+
 func main() {
 	http.HandleFunc("/", sayHello)
+	http.HandleFunc("/products", productController)
 	fmt.Println("Server is listenging on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello From Go!")
+}
+
+// list / get data
+func productController(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	result, _ := json.Marshal(products)
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
 }
